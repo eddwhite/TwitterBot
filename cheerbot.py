baa8915e182064de648b38ef588ed90e42440a92
eddwhite <eddwhite1@gmail.com>
2015-10-22 22:59:57 +0100

    Key separation
    Put secret keys into a separate python file and added that to
    .gitignore. Phew, that was a close one!

--- cheerbot.py
+++ cheerbot.py
@@ -2,14 +2,11 @@
 # http://videlais.com/2015/03/02/how-to-create-a-basic-twitterbot-in-python/
 
 import tweepy
+from keys import *
 
 class TwitterAPI:
     def __init__(self):
-        consumer_key = ""
-        consumer_secret = ""
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        access_token = ""
-        access_token_secret = ""
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 
