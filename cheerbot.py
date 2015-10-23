6d7253b7486b66d2775b68c39990a29ea267c4c7
eddwhite <eddwhite1@gmail.com>
2015-10-23 11:30:04 +0100

    Improve conversation support
    Can now identify if a mention is in reply to a bot status. Code to check
    if a new user is mentioned still in progress

--- cheerbot.py
+++ cheerbot.py
@@ -22,15 +22,53 @@ class TwitterAPI:
 	def get_mentions(self):
 		last_tweet = self.api.me().status.id
 		mentions = self.api.mentions_timeline(since_id=last_tweet)
-		for mention in mentions:
-			print(mention.user.screen_name)
-			print(mention.text)
+		#for mention in mentions:
+		#	print(mention.user.screen_name)
+		#	print(mention.text)
 		return mentions
-		
+
+	def get_my_username(self):
+		return self.api.auth.get_username()
+
+	def get_tweet(self, status_id):
+		return self.api.get_status(id=status_id)
+
+def get_users_mentioned(text):
+	users=[]
+	for word in text.split():
+		if word.startswith('@'):
+			users.append(word[1:])
+	return users
+
+def new_user_mentioned(api, mention):
+	users = get_users_mentioned(mention.text)
+	users.remove(api.get_my_username())
+	prev_status = api.get_tweet(mention.in_reply_to_status_id_str)
+	prev_users = get_users_mentioned(prev_status.text)
+	#need to remove the original person mentioned
+	print(prev_users)
+	new_users = set(users) - set(prev_users)
+	if not new_users:
+		return False
+	else:
+		return True
+
+def create_reply_string(api, mention):
+	# If in reply to a tweet by bot, change string!
+	string = ""
+	if mention.in_reply_to_screen_name == api.get_my_username():
+		if new_user_mentioned(api, mention):
+			string = "Thanks for spreading the word!"
+		else:
+			string = "You can tweet at me all you like, I can't cheer you up quite yet!"
+	else:
+		string = "I'm not quite ready yet, but I'm sure my creator Edd will say when I'm done!"
+	return string
 
 if __name__ == "__main__":
 	twitter = TwitterAPI()
 	mentions = twitter.get_mentions()
 	for mention in mentions:
-		reply_str = "I'm not ready yet!!!"
-		twitter.reply(mention.id, mention.user.screen_name, reply_str)
+		reply_str = create_reply_string(twitter, mention)
+		print(reply_str)
+		#twitter.reply(mention.id, mention.user.screen_name, reply_str)
